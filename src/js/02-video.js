e1814f118f398da5cdd29cfacd53c6c64bd60198
Natalia Khodorova <icxodnik@gmail.com>
2024-02-09 02:28:33 +0100

    part 2

---
+++ src/js/02-video.js
@@ -0,0 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeKey = 'videoplayer-current-time';
+const initSeconds = localStorage.getItem(timeKey);
+
+if (initSeconds) {
+    player.setCurrentTime(initSeconds);
+}
+
+function saveCurrentTime(seconds) {
+    localStorage.setItem(timeKey, seconds);
+    console.log(seconds)
+}
+
+const saveTime = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', data => {
+    saveTime(data.seconds);
+});
